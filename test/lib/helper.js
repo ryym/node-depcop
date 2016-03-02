d4c3875ea899ff7304f22baa5beaaf6fe38421b6
ryym <ryym.64@gmail.com>
2016-03-02 13:19:13 +0900

    :lock: Add helper method

--- test/lib/helper.js
+++ test/lib/helper.js
@@ -4,3 +4,12 @@ import path from 'path';
  * The absolute path of fixtures directory.
  */
 export const FIXTURES_PATH = path.resolve(__dirname, './fixtures');
+
+/**
+ * Resolve given paths from {@link FIXTURES_PATH}.
+ * @param {string...} paths
+ * @return {string} The absolute path.
+ */
+export function fixturePath(...paths) {
+  return path.resolve(FIXTURES_PATH, ...paths);
+}
