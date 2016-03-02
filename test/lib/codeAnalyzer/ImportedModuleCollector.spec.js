b237c8f48c115f9aaba6a29f5dbf05f0173aa975
ryym <ryym.64@gmail.com>
2016-03-02 22:43:07 +0900

    :sparkles::lock: ModuleCollector: Add unit tests and jsdoc

---
+++ test/lib/codeAnalyzer/ImportedModuleCollector.spec.js
@@ -0,0 +1,82 @@
+import assert from 'power-assert';
+import forEach from 'mocha-each';
+import FileInfo from '$lib/FileInfo';
+import ImportedModuleCollector from '$lib/codeAnalyzer/ImportedModuleCollector';
+
+/** @test {ImportedModuleCollector} */
+describe('ImportedModuleCollector', () => {
+  const collector = new ImportedModuleCollector({
+    ecmaVersion: 6,
+    sourceType: 'module'
+  });
+
+  beforeEach(() => {
+    collector.clearFoundModules();
+  });
+
+  forEach([
+    ['recognizes \'import\'s', {
+      code: `
+        import foo from 'foo';
+        import bar from 'bar';
+      `,
+      modules: ['foo', 'bar']
+    }],
+    ['ignores relative paths', {
+      code: `
+        import foo from 'foo';
+        import bar from './bar';
+        import baz from '../baz';
+        import qux from './some/dir/qux';
+        import quux from '../../../quux';
+      `,
+      modules: ['foo']
+    }]
+  ])
+  .it('collects module information: %s', (_, data) => {
+    collector.searchImports(data.code, FileInfo.asAnonymous());
+    const modules = collector.collectImportedModules();
+
+    assert.deepEqual(
+      modules.map(m => m.getName()),
+      data.modules
+    );
+  });
+
+  it('colletcs modules with a file information that uses the modules', () => {
+    [
+      [
+        FileInfo.asLib('a.js'), `
+          import module1 from 'module1';
+          import module2 from 'module2'
+        `
+      ], [
+        FileInfo.asLib('b.js'), `
+          import module1 from 'module1';
+          import module3 from 'module3';
+        `
+      ], [
+        FileInfo.asLib('c.js'), `
+          import module2 from 'module2';
+          import module3 from 'module3';
+        `
+      ]
+    ]
+    .forEach(([fileInfo, code]) => {
+      collector.searchImports(code, fileInfo);
+    });
+
+    const modules = collector.collectImportedModules()
+      .reduce((ms, module) => {
+        const paths = module.getDependents().map(f => f.getPath());
+        ms[module.getName()] = paths;
+        return ms;
+      }, {});
+
+    assert.deepEqual(modules, {
+      module1: ['a.js', 'b.js'],
+      module2: ['a.js', 'c.js'],
+      module3: ['b.js', 'c.js']
+    });
+  });
+});
