0fb00c885ed53b4e148335e49a377ddd9f537d03
ryym <ryym.64@gmail.com>
2016-04-03 09:52:12 +0900

    :wrench: Upgrade ESLint and remove babel-eslint
    Using 'parserOptions', ESLint can parse code written in ES2015
    without babel-eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   "root": true,
-  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
   "extends": "eslint:recommended",
   "env": {
     "node": true,
